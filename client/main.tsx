5f54567742eb8e2eb1c346d346e2134d7aefda0e
Yau Yen Ching <yauyenching@pm.me>
2022-09-10 16:57:15 +0800

    Add missing main.css and main.html files

--- client/main.tsx
+++ client/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from '../imports/ui/App';
 
 Meteor.startup(() => {
-  // const container = document.getElementById('react-target') as HTMLElement;
-  const root = createRoot(document.getElementById('react-target'));
+  const container = document.getElementById('react-target');
+  const root = createRoot(container); 
   root.render(<App />);
 });
