c242f7caf57a5c83c856fa3b928fdc472b3e520f
Yau Yen Ching <yauyenching@pm.me>
2022-09-17 15:02:35 +0800

    Finish static floorplan with room toggling

--- imports/ui/FloorPlan.tsx
+++ imports/ui/FloorPlan.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, Box } from "@mui/material";
 import { RoomId } from '../db/temps';
 import { calculateAverageTemps } from '../utils/avgTemp';
+// import './FloorPlanStyle.css';
 
 export default function FloorPlan(
   { visibleRooms, handleToggleVisibleRooms, roomTemps }
@@ -10,18 +11,45 @@ export default function FloorPlan(
   const roomAvgTemps: readonly number[] = calculateAverageTemps(roomTemps);
   // console.log(roomAvgTemps);
 
-  return (
-    <Stack direction="row" spacing={2}>
-      {roomIds.map(roomId =>
-        <Button
-          key={roomId}
-          variant="contained"
-          onClick={() => {
-            handleToggleVisibleRooms(roomId);
-          }}
+  const rooms: JSX.Element[] =
+    roomIds.map(roomId => {
+      const roomClass: string =
+        roomId !== 0 ? `standard room R${roomId}` : 'big room';
+      return (
+        <div
+          className={roomClass}
+          onClick={() => { handleToggleVisibleRooms(roomId) }}
         >
-          Room {roomId} ({(!isNaN(roomAvgTemps[roomId]) ? roomAvgTemps[roomId] : 0).toFixed(2)})
-        </Button>)}
-    </Stack>
+          R{roomId}
+        </div>
+      );
+    })
+
+  return (
+    <>
+      <div>
+        <Stack spacing={2}>
+          <Stack direction="row" spacing={2}>
+            {roomIds.map(roomId =>
+              <Button
+                key={roomId}
+                variant="contained"
+                onClick={() => {
+                  handleToggleVisibleRooms(roomId);
+                }}
+              >
+                Room {roomId} ({(!isNaN(roomAvgTemps[roomId]) ? roomAvgTemps[roomId] : 0).toFixed(2)})
+              </Button>)}
+          </Stack>
+        </Stack>
+      </div>
+      <div className='base floor-plan'>
+        <div className='column decorative' />
+        <div className='closet decorative' style={{ "left": "18px" }} />
+        <div className='closet decorative' style={{ "left": "95px" }} />
+        <div className='decorative-room' />
+        {rooms}
+      </div>
+    </>
   );
 }
